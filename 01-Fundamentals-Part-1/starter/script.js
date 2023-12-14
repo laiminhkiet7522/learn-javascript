4ca8e4e0e4e872df318c03324794eb47e497067d
Minh Kiet <kietminh070502@gmail.com>
2023-12-14 13:56:05 +0700

    Section 2: JavaScript Fundamentals - Values and Variables

---
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,35 @@
+let js = 'amazing';
+if (js === 'amazing') {
+  console.log('amazing');
+}
+
+// Allow
+// let variables = 1;
+// let _variables = 1;
+// let $variables = 1;
+
+// Not Allow
+// let 4variables = 1;
+// let new = 1;
+
+let person = "Kiet";
+let PI = 3.1415;
+
+let myFirstJob = "Programmer";
+let myCurrentJob = "Teacher";
+
+let job1 = "programmer";
+let job2 = "teacher";
+
+// LECTURE: Values and Variables
+console.log("----------LECTURE: Values and Variables----------");
+// 1. Declare variables called 'country', 'continent' and 'population' and
+// assign their values according to your own country(population in millions)
+let country = "Viet Nam";
+let continent = "Asia";
+let population = "90 million people";
+
+// 2. Log their values to the console
+console.log(country);
+console.log(continent);
+console.log(population);
