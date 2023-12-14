d1dc3dea998cd52263981a044d557b0bb08d5675
Minh Kiet <kietminh070502@gmail.com>
2023-12-14 14:38:52 +0700

    Section 2: JavaScript Fundamentals - Data Types

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,35 +1,46 @@
-let js = 'amazing';
-if (js === 'amazing') {
-  console.log('amazing');
-}
-
-// Allow
-// let variables = 1;
-// let _variables = 1;
-// let $variables = 1;
-
-// Not Allow
-// let 4variables = 1;
-// let new = 1;
-
-let person = "Kiet";
-let PI = 3.1415;
-
-let myFirstJob = "Programmer";
-let myCurrentJob = "Teacher";
-
-let job1 = "programmer";
-let job2 = "teacher";
-
 // LECTURE: Values and Variables
 console.log("----------LECTURE: Values and Variables----------");
-// 1. Declare variables called 'country', 'continent' and 'population' and
-// assign their values according to your own country(population in millions)
+
+console.log("1. Declare variables called 'country', 'continent' and 'population' and assign their values according to your own country(population in millions)");
 let country = "Viet Nam";
 let continent = "Asia";
 let population = "90 million people";
 
-// 2. Log their values to the console
+console.log("2. Log their values to the console");
 console.log(country);
 console.log(continent);
 console.log(population);
+
+// LECTURE: Data Types
+console.log("----------LECTURE: Data Types----------");
+
+let javascriptIsFun = true;
+
+console.log(javascriptIsFun);
+console.log(typeof javascriptIsFun);
+console.log(typeof 17);
+console.log(typeof true);
+console.log(typeof "Kiet");
+
+javascriptIsFun = 'YES';
+console.log(javascriptIsFun);
+
+let year;
+console.log(year);
+console.log(typeof year);
+
+year = 2023;
+console.log(year);
+console.log(typeof year);
+
+console.log("1. Declare a variable called 'isIsland' and set its value according to your country. The variable should hold a Boolean value. Also declare a variable 'language', but don't assign it any value yet");
+let isIsLand = "Viet Nam";
+population = 90;
+country = "Ho Chi Minh city";
+let language;
+
+console.log("2. Log the types of 'isIsland', 'population', 'country' and 'language' to the console");
+console.log(typeof isIsLand);
+console.log(typeof population);
+console.log(typeof country);
+console.log(typeof language);
