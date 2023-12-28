8d0342079c17cf946efc858e196014cf4eef51bf
Minh Kiet <kietminh070502@gmail.com>
2023-12-28 15:11:22 +0700

    Section 2: JavaScript Fundamentals - Logical Operators

--- 01-Fundamentals-Part-1/script.js
+++ 01-Fundamentals-Part-1/script.js
@@ -299,3 +299,36 @@
 // }
 
 // console.log("8. Reflect on why we should use the === operator and type conversion in this situation");
+
+
+// LECTURE: Logical Operators
+// console.log("----------LECTURE: Logical Operators----------");
+
+// const hasDriversLicense = true;
+// const hasGoodVision = false;
+
+// console.log(hasDriversLicense && hasGoodVision);
+// console.log(hasDriversLicense || hasGoodVision);
+// console.log(!hasDriversLicense);
+
+// const shouldDrive = hasDriversLicense && hasGoodVision;
+// if (shouldDrive) {
+//   console.log("Sarah is able to drive");
+// } else {
+//   console.log("Sarah is not able to drive");
+// }
+
+// console.log("1. Comment out the previous code so the prompt doesn't get in the way");
+// console.log("2. Let's say Sarah is looking for a new country to live in. She wants to live in a country that speaks english, has less than 50 million people and is not an island.");
+// console.log("3. Write an if statement to help Sarah figure out if your country is right for her. You will need to write a condition that accounts for all of Sarah's criteria. Take your time with this, and check part of the solution if necessary.");
+// console.log("4. If yours is the right country, log a string like this: 'You should live in Portugal :)'.If not, log 'Portugal does not meet your criteria :('");
+// console.log("5. Probably your country does not meet all the criteria.So go back and temporarily change some variables in order to make the condition true(unless you live in Canada : D) ");
+// let language = 'english';
+// let population = 49;
+// let island = false;
+
+// if (language == 'english' && population < 50 && !island) {
+//   console.log("You should live in Portugal :)");
+// } else {
+//   console.log("Portugal does not meet your criteria :(");
+// }
