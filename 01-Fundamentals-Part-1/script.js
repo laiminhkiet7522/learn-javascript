9756aacd8b4dc607f534221cac5cc2a2d5b556c0
Minh Kiet <kietminh070502@gmail.com>
2024-01-03 16:04:22 +0700

    Section 2: JavaScript Fundamentals - The Conditional (Ternary) Operator

--- 01-Fundamentals-Part-1/script.js
+++ 01-Fundamentals-Part-1/script.js
@@ -385,3 +385,22 @@
 //   default:
 //     console.log('Great language too :D');
 // }
+
+
+// LECTURE: LECTURE: The Conditional (Ternary) Operator
+// console.log("----------LECTURE: The Conditional (Ternary) Operator----------");
+
+// const age = 21;
+// age >= 18 ? console.log('I like to drink wine') : console.log('I like drink water');
+
+// console.log(`I like to drink ${age >= 18 ? 'wine 🍷' : 'water 💧'}`);
+
+// console.log("1. If your country's population is greater than 33 million, use the ternary operator to log a string like this to the console: 'Portugal's population is above average'. Otherwise, simply log 'Portugal's population is below average'. Notice how only one word changes between these two sentences!");
+
+// let population = 30;
+// population > 33 ? console.log("Portugal's population is above average") : console.log("Portugal's population is below average");
+
+// console.log("2. After checking the result, change the population temporarily to 13 and then to 130. See the different results, and set the population back to original");
+
+// population = 90;
+// population > 33 ? console.log("Portugal's population is above average") : console.log("Portugal's population is below average");
