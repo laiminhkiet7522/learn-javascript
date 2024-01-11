816c984cc6f0492f44cab1b1a6593cf3c3af6e27
Minh Kiet <kietminh070502@gmail.com>
2024-01-11 14:15:57 +0700

    Section 3: JavaScript Fundamentals - Functions

--- 02-Fundamentals-Part-2/script.js
+++ 02-Fundamentals-Part-2/script.js
@@ -1,11 +1,34 @@
 'use strict';
 
-let hasDriversLicense = false;
-const passTest = true;
+// let hasDriversLicense = false;
+// const passTest = true;
 
-if (passTest) {
-  hasDriversLicense = true;
+// if (passTest) {
+//   hasDriversLicense = true;
+// }
+// if (hasDriversLicense) {
+//   console.log('I can drive');
+// }
+
+
+function logger() {
+  console.log('My name is Minh Kiet');
 }
-if (hasDriversLicense) {
-  console.log('I can drive');
+//Caling / Running / Invoking function
+logger();
+logger();
+logger();
+
+function fruitProcessor(apples, oranges) {
+  console.log(apples, oranges);
+  const juice = `Juice with ${apples} apples and ${oranges} oranges.`;
+  return juice;
 }
+
+const appleJuice = fruitProcessor(5,0);
+console.log(appleJuice);
+
+console.log(fruitProcessor(5, 0));
+
+const num = Number('23');
+console.log(typeof (num));
