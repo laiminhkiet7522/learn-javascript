c6f3b3ef0ab8ec2f4d8357bb945f59a1158cb133
Minh Kiet <kietminh070502@gmail.com>
2024-01-11 14:31:39 +0700

    Section 3: JavaScript Fundamentals - Function Declarations vs. Expressions

--- 02-Fundamentals-Part-2/script.js
+++ 02-Fundamentals-Part-2/script.js
@@ -11,24 +11,39 @@
 // }
 
 
-function logger() {
-  console.log('My name is Minh Kiet');
-}
-//Caling / Running / Invoking function
-logger();
-logger();
-logger();
+// function logger() {
+//   console.log('My name is Minh Kiet');
+// }
+// //Caling / Running / Invoking function
+// logger();
+// logger();
+// logger();
 
-function fruitProcessor(apples, oranges) {
-  console.log(apples, oranges);
-  const juice = `Juice with ${apples} apples and ${oranges} oranges.`;
-  return juice;
-}
+// function fruitProcessor(apples, oranges) {
+//   console.log(apples, oranges);
+//   const juice = `Juice with ${apples} apples and ${oranges} oranges.`;
+//   return juice;
+// }
 
-const appleJuice = fruitProcessor(5,0);
-console.log(appleJuice);
+// const appleJuice = fruitProcessor(5,0);
+// console.log(appleJuice);
 
-console.log(fruitProcessor(5, 0));
+// console.log(fruitProcessor(5, 0));
 
-const num = Number('23');
-console.log(typeof (num));
+// const num = Number('23');
+// console.log(typeof (num));
+
+
+//Function Declaration
+// function calcAge1(birthYear) {
+//   return 2023 - birthYear;
+// }
+// const age1 = calcAge1(2002);
+
+// //Function Expression
+// const calcAge2 = function (birthYear) {
+//   return 2023 - birthYear;
+// }
+// const age2 = calcAge2(2002);
+
+// console.log(age1, age2);
