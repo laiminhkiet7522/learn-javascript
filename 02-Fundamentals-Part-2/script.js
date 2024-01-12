2a585d62c2b296860e06f7dad9086ca60c6f6ca6
Minh Kiet <laiminhkiet07052002@gmail.com>
2024-01-12 14:17:38 +0700

    Section 3: JavaScript Fundamentals - Functions Calling Other Functions

--- 02-Fundamentals-Part-2/script.js
+++ 02-Fundamentals-Part-2/script.js
@@ -62,3 +62,18 @@
 // }
 // console.log(yearUntilRetirement(2002,'Kiet'));
 // console.log(yearUntilRetirement(2003,'Meo'));
+
+
+function cutFruitPieces(fruit){
+  return fruit * 4;
+}
+
+function fruitProcessor(apples, oranges) {
+  const applePieces = cutFruitPieces(apples);
+  const orangePieces = cutFruitPieces(oranges);
+
+  const juice = `Juice with ${applePieces} pieces of apple and ${orangePieces} piece of orange.`;
+  return juice;
+}
+
+console.log(fruitProcessor(5, 10));
