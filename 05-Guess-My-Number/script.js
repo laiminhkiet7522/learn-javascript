b077eca4814acdfd646e41c7bb9772578a9bc3da
Minh Kiet <laiminhkiet07052002@gmail.com>
2024-01-23 14:47:12 +0700

    Section 7: Selecting and Manipulating Elements

--- 05-Guess-My-Number/script.js
+++ 05-Guess-My-Number/script.js
@@ -1,3 +1,12 @@
 'use strict';
 const temp = document.querySelector('.message').textContent;
 console.log(temp);
+
+document.querySelector('.message').textContent = 'Correct Number';
+
+document.querySelector('.number').textContent = 13;
+document.querySelector('.score').textContent = 10;
+
+// Get the value in the input field
+document.querySelector('.guess').value = 23;
+console.log(document.querySelector('.guess').value);
