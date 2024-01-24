d6c4dd21ad85aef7acbe7d6ad484cff0476957dc
Minh Kiet <laiminhkiet07052002@gmail.com>
2024-01-24 14:47:35 +0700

    Section 7: Handling Click Events

--- 05-Guess-My-Number/script.js
+++ 05-Guess-My-Number/script.js
@@ -1,4 +1,5 @@
 'use strict';
+/*
 const temp = document.querySelector('.message').textContent;
 console.log(temp);
 
@@ -10,3 +11,13 @@ document.querySelector('.score').textContent = 10;
 // Get the value in the input field
 document.querySelector('.guess').value = 23;
 console.log(document.querySelector('.guess').value);
+*/
+
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
+  console.log(guess, typeof guess);
+
+  if (!guess) {
+    document.querySelector('.message').textContent = '⛔ No Number';
+  }
+});
