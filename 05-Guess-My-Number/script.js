bd023eca4cbfa9e6b0ccce82428f778340f2d455
Minh Kiet <laiminhkiet07052002@gmail.com>
2024-01-24 15:29:51 +0700

    Section 7: Manipulating CSS Styles

--- 05-Guess-My-Number/script.js
+++ 05-Guess-My-Number/script.js
@@ -22,13 +22,18 @@ document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
   console.log(guess, typeof guess);
 
+  //When there is no input
   if (!guess) {
     document.querySelector('.message').textContent = '⛔ No Number';
+
+    //When player wins
   } else if (guess === secretNumber) {
     document.querySelector('.message').textContent = '🎉 Correct Number';
     document.querySelector('body').style.backgroundColor = '#60b347';
     document.querySelector('.number').style.width = '30rem';
     document.querySelector('.number').textContent = secretNumber;
+
+    //When guess is too high
   } else if (guess > secretNumber) {
     if (score > 1) {
       document.querySelector('.message').textContent = '📈 Too High';
@@ -36,9 +41,11 @@ document.querySelector('.check').addEventListener('click', function () {
       document.querySelector('.score').textContent = score;
     } else {
       document.querySelector('.message').textContent = '💥 You lost the game';
+      document.querySelector('body').style.backgroundColor = '#FF0000';
       document.querySelector('.score').textContent = 0;
     }
 
+    //When guess is too low
   } else if (guess < secretNumber) {
     if (score > 1) {
       document.querySelector('.message').textContent = '📉 Too Low';
@@ -46,6 +53,7 @@ document.querySelector('.check').addEventListener('click', function () {
       document.querySelector('.score').textContent = score;
     } else {
       document.querySelector('.message').textContent = '💥 You lost the game';
+      document.querySelector('body').style.backgroundColor = '#FF0000';
       document.querySelector('.score').textContent = 0;
     }
   }
